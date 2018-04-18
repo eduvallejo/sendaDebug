04035289b8582a87691c505d43eeaeaf22e717ab
eduvallejo <vallejoprietoeduardo@gmail.com>
2018-04-18 20:30:43 +0200

    primer commit firstspecie

--- js/4oscillator.js
+++ js/4oscillator.js
@@ -13,24 +13,8 @@ var setIntervalNotasSoprano;
 //compressor
 var compressor = context.createDynamicsCompressor();
 var compressorSoprano = context.createDynamicsCompressor();
-// compressor.threshold.setValueAtTime(-50, context.currentTime);
-// compressor.knee.setValueAtTime(40, context.currentTime);
-// compressor.ratio.setValueAtTime(12, context.currentTime);
-// compressor.attack.setValueAtTime(0, context.currentTime);
-// compressor.release.setValueAtTime(0.25, context.currentTime);
 
-// connect the AudioBufferSourceNode to the destination
-// source.connect(context.destination);
 
-// button.onclick = function() {
-//   var active = button.getAttribute('data-active');
-//   if(active == 'false') {
-//     button.setAttribute('data-active', 'true');
-//     button.innerHTML = 'Remove compression';
-
-//     source.disconnect(context.destination);
-//     source.connect(compressor);
-//     compressor.connect(context.destination);
 
 function oscillatorFunction() {
 	if (counterOscillator >= longitudCantus) {
@@ -44,6 +28,7 @@ function oscillatorFunction() {
 			oscillator.stop();
 			oscillatorSoprano.stop();
 		}
+		//oscillator cantus
 		oscillator = context.createOscillator();
 		gainNode = context.createGain();
 		gainNode.gain.value = 1;
@@ -66,6 +51,7 @@ function oscillatorFunction() {
 		// oscillator.stop();
 		counterOscillator++;
 
+		//oscillator cantus
 		oscillatorSoprano = context.createOscillator();
 		gainNodeSoprano = context.createGain();
 		gainNodeSoprano.gain.value = 1;
@@ -78,10 +64,10 @@ function oscillatorFunction() {
 		//el primer param es a lo q tiende de 0% a 100%, el segundo cuando empieza, , el tercero es la velocidad a la q tiende(0max 1min)
 		
 		compressorSoprano.threshold.setValueAtTime(-50, context.currentTime);
-		compressorSoprano.knee.setValueAtTime(40, context.currentTime);
-		compressorSoprano.ratio.setValueAtTime(12, context.currentTime);
-		compressorSoprano.attack.setValueAtTime(0, context.currentTime);
-		compressorSoprano.release.setValueAtTime(0.25, context.currentTime);
+		// compressorSoprano.knee.setValueAtTime(40, context.currentTime);
+		// compressorSoprano.ratio.setValueAtTime(12, context.currentTime);
+		// compressorSoprano.attack.setValueAtTime(0, context.currentTime);
+		// compressorSoprano.release.setValueAtTime(0.25, context.currentTime);
 
 		gainNodeSoprano.connect(compressorSoprano);
 		// audioSong.play();
